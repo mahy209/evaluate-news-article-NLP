b10b940af15a174b6c43539944bf8b5f18b2fca9
mahy209 <60543525+mahy209@users.noreply.github.com>
2021-08-29 02:15:40 +0200

    final  touches

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,12 @@
+import { handleSubmit } from './js/formHandler'
+import { checkForUrl } from './js/urlChecker'
+
 // scss files
 import './styles/main.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-
-import { handleSubmit } from './js/formHandler'
-import { checkForUrl } from './js/urlChecker'
-
-alert (" HELLO THERE! ")
 
 export { 
     handleSubmit,
